a03c50d99c8b561020fbf764a0082e127dbae702
dylankrish <91637207+dylankrish@users.noreply.github.com>
2025-06-24 19:38:01 -0400

    reorganize variables

--- backup.sh
+++ backup.sh
@@ -6,9 +6,6 @@ DIR=""
 # name of backup
 BACKUP_NAME="backup"
 
-# timestamp for folder name
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-
 # ssh key to use
 SSH_KEY="~/.ssh/backup_rsync"
 # remote backup server config
@@ -16,10 +13,12 @@ REMOTE_USER="backupuser"
 REMOTE_HOST="backupserver.local"
 REMOTE_DIR="/home/backupuser/backups"
 
-set -x # verbose
-set -e # exit on error
 
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S") # timestamp for folder name
 ARCHIVE_NAME="$BACKUP_NAME-$TIMESTAMP.tar.gz"
+
+set -e # exit on error
+set -x # verbose
 
 # compress directory for backup
 echo "Compressing $DIR into $ARCHIVE_NAME..."
