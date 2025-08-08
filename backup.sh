517624bf8b194c09ea1ec1b6b24a6723fcce4428
Dylan Krishnan <91637207+dylankrish@users.noreply.github.com>
2025-08-07 23:27:16 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,20 +7,26 @@ REMOTE_USER="backupuser" # user to log in as
 REMOTE_HOST="backupserver.local" # hostname or IP address to connect to
 REMOTE_DIR="/" # directory to back up to on the server, remember to add / to the end
 
-# add any pre-backup code here, such as temporarily stopping docker containers
-# ---
-
-# ---
-
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S") # timestamp for folder name
 ARCHIVE_NAME="$BACKUP_NAME-$TIMESTAMP.tar.gz"
 
 set -e # exit on error
 # set -x # verbose
 
+# add any pre-backup code here, such as temporarily stopping services to prevent file changes
+# ---
+
+# ---
+
 # compress directory for backup
 echo "Compressing $DIR into $ARCHIVE_NAME..."
 tar -czf "$ARCHIVE_NAME" "$DIR"
+
+# add any restore code here, such as restarting the services
+# ---
+
+# ---
+
 
 echo "Syncing to remote server..."
 rsync -avz -e "ssh -i $SSH_KEY" "$ARCHIVE_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
